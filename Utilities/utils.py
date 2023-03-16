6dce80341c4fb7e9da81d9d7dae22459d48ec032
Naykat <65075625+Naykat@users.noreply.github.com>
2023-03-16 21:40:36 +0300

    Utils refactoring

--- Utilities/utils.py
+++ Utilities/utils.py
@@ -16,13 +16,13 @@ def prepare_coordinates(width, height):
 def encode_last_symbol_coordinates(image, width, height):
     prepared_coordinates = prepare_coordinates(width, height)
     for current_pixel in range(4):
-        r,g = image.getpixel((current_pixel, 0))[:2]
-        image.putpixel((current_pixel, 0),
+        r,g = image.getpixel((current_pixel+1, 0))[:2]
+        image.putpixel((current_pixel+1, 0),
                        (r,g,prepared_coordinates[current_pixel]))
 
 def decode_last_symbol_coordinates(image):
     encoded_coordinates = []
-    for current_pixel in range(4):
+    for current_pixel in range(1,5):
         encoded_coordinates.append(image.getpixel((current_pixel, 0))[2])   
     width = encoded_coordinates[0]*255+encoded_coordinates[1]
     height = encoded_coordinates[2]*255+encoded_coordinates[3]
