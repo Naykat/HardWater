cf9ea26df7315eed131dddbffbe4d0b92693552e
Naykat <65075625+Naykat@users.noreply.github.com>
2023-03-17 18:01:45 +0300

    Added Watermark Availability Check Functions

--- Utilities/MathFunctions.py
+++ Utilities/MathFunctions.py
@@ -1,8 +1,20 @@
 #MathFunctions
 from math import ceil
 
+def get_mark_position(x: int, y: int) -> tuple:
+    return int(x*0.15), ceil(y*0.85)
+
 def get_step_position(x: int, y: int) -> tuple: 
     return ceil(x*0.85), int(y*0.15)
+
+def create_model_values(rgb: tuple) -> list:
+    model_rgb = []
+    for colour in rgb:
+        if colour==255:
+            model_rgb.append(254)
+        else:
+            model_rgb.append(colour+1)
+    return model_rgb
 
 def calculate_step(text: str, x: int, y: int) -> int: 
     if len(text)>max(x,y):
