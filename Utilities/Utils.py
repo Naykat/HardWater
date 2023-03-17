c03fa5e32feed55092e455cb8947d9bb573186ce
Naykat <65075625+Naykat@users.noreply.github.com>
2023-03-17 18:02:41 +0300

    Added: mark(), is_watermarked()

--- Utilities/Utils.py
+++ Utilities/Utils.py
@@ -1,6 +1,6 @@
 #Utilities
 from PIL import Image
-from Utilities.MathFunctions import get_step_position
+from Utilities.MathFunctions import get_step_position, get_mark_position, create_model_values
 
 def get_type(variable) -> str:
     return str(type(variable)).split("'")[1]
@@ -15,3 +15,22 @@ def get_step(image: Image) -> int:
     width, height = image.size
     step_position = get_step_position(width, height)
     return image.getpixel(step_position)[2]
+
+def mark(image: Image) -> None:
+    width, height = image.size
+    mark_x, mark_y = get_mark_position(width, height)
+    model_r,model_g, model_b = image.getpixel((mark_x+1, mark_y))
+    r,g,b = create_model_values((model_r,model_g, model_b))
+    image.putpixel((mark_x,mark_y),(r,g,b))
+
+def is_watermarked(image: Image) -> bool:
+    width, height = image.size
+    mark_x, mark_y = get_mark_position(width, height)
+    model_r, model_g, model_b  = image.getpixel((mark_x+1, mark_y))
+    r,g,b = image.getpixel((mark_x, mark_y))
+    if abs(model_r-r) == 1 \
+       and abs(model_b-b) == 1 \
+       and abs(model_b-b) == 1:
+        return True
+    else:
+        return False
